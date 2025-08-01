8afce037400327b0c425f7ab5e5a7a0a0136cdbe
Joaquin <xvitkleo@gmail.com>
2025-08-01 10:07:14 -0500

    fix

--- app.js
+++ app.js
@@ -10,12 +10,6 @@ app.options('*', cors({ origin: '*' }));
 app.use(express.json());
 app.use(express.static('public'));
 
-app.get('/api/1.0/test-cors', (req, res) => {
-  res.json({ ok: true });
-});
-
-app.use('/api/1.0', require('./app/routes'));
-
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header(
@@ -28,6 +22,12 @@ app.use((req, res, next) => {
   );
   next();
 });
+
+app.get('/api/1.0/test-cors', (req, res) => {
+  res.json({ ok: true });
+});
+
+app.use('/api/1.0', require('./app/routes'));
 
 app.use((req, res) => {
   res.status(404).send({ error: 'Not found' });
