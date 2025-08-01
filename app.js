c6ba6d2515e040696d4d2c51492c8dc2caf66523
Joaquin <xvitkleo@gmail.com>
2025-08-01 10:09:59 -0500

    fix

--- app.js
+++ app.js
@@ -2,13 +2,6 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const { dbConnect } = require('./config/mongo');
-
-const PORT = process.env.PORT || 3000;
-app.use(cors({ origin: '*' }));
-app.options('*', cors({ origin: '*' }));
-app.use(express.json());
-app.use(express.static('public'));
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
@@ -20,8 +13,17 @@ app.use((req, res, next) => {
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept, Authorization',
   );
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
   next();
 });
+
+const PORT = process.env.PORT || 3000;
+app.use(cors({ origin: '*' }));
+app.options('*', cors({ origin: '*' }));
+app.use(express.json());
+app.use(express.static('public'));
 
 app.get('/api/1.0/test-cors', (req, res) => {
   res.json({ ok: true });
