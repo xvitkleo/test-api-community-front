af71bb7ac8ede97ac6dbb719c301c4f60321fcc6
Joaquin <xvitkleo@gmail.com>
2025-08-01 09:26:09 -0500

    cors allow all

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ const app = express()
 const { dbConnect } = require('./config/mongo')
 
 const PORT = process.env.PORT || 3000
-app.use(cors())
+app.use(cors({ origin: '*' }))
 app.use(express.json())
 app.use(express.static('public'));
-app.use('/api/1.0', require('./app/routes'))
+app.use('/', require('./app/routes'))
 
 app.listen(PORT, () => {
-    console.log(`Tu API es http://localhost:${PORT}/api/1.0`)
+    console.log(`Tu API es http://localhost:${PORT}/`)
 })
